3636e9885cbd380ef4e7fad5c46d24e6a419ee48
Ben <benbuttons@buttonsnmore.com>
2024-05-11 23:27:58 -0400

    Fix petname with bad args

--- petname/petname.sh
+++ petname/petname.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 petname() {
+	USAGE="Usage: petname [-d DELIMITER] [WORD_COUNT]"
 	DELIM=' '
-	local WORDS=1
+	WORDS=1
+	WORDLISTS=( $(find /usr/share/dict -maxdepth 1 -type f -o -type l) )
 	while ! [[ -z $1 ]]; do
-		if [[ ${1:0:2} == -d ]]; then
+		if [[ ${1:0:2} == -d ]] || [[ $1 == --delimiter ]]; then
 			DELIM="${1:2}"
 			[[ ${#DELIM} == 0 ]] && DELIM="$2" && shift 
+		elif [[ ${1:0:2} == -h ]] || [[ $1 == --help ]]; then
+			echo $USAGE
+			return
 		else
-			WORDS="$1"
+			WORDS="$(tr -cd '[:digit:]' <<<$1)"
 		fi
 		shift
 	done
 	# Common pre-installed wordlists
-	WORDLISTS=( $(find /usr/share/dict -maxdepth 1 -type f -o -type l) )
-	shuf -n $WORDS ${WORDLISTS[0]} | sed "s:'::g;"'s:[-&_+.\\]::g;' | paste -sd"$DELIM"
+	shuf -n $WORDS ${WORDLISTS[0]} | \
+		sed "s:'::g;"'s:[-&_+.\\]::g;' | \
+		paste -sd"$DELIM" 2>/dev/null || \
+	echo $USAGE
 }
 [[ -z $* ]] && export -f petname || petname "$@"
